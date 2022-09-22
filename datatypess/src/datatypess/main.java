9db52d504199943ef55b1071cabc4e1f0f61784b
sevdegül arkan <sevdegl arkan@sevde>
2022-09-22 12:27:31 +0300

    java

---
+++ datatypess/src/datatypess/main.java
@@ -0,0 +1,13 @@
+package datatypess;
+
+public class main {
+
+	public static void main(String[] args) {
+	 
+		int ogrenciSayısı=12;
+		System.out.println("oğrenci sayısı: "+ ogrenciSayısı );
+		System.out.println("oğrenci sayısı: "+ ogrenciSayısı );
+		
+	}
+
+}
